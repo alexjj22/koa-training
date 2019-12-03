f23a7e0abec95610e63cca0981dbf3813c8d566b
alexjj22griddynamics <arastvorov@griddynamics.com>
2019-12-03 15:24:37 +0200

    add request body parser

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const Koa = require('koa');
 const app = new Koa();
 
 require('./handlers/01-favicon').init(app);
+require('./handlers/02-bodyParser').init(app);
 
 app.use(async ctx => {
   ctx.body = 'Hello, World!';
