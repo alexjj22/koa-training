cc4ce6807eb90c7970a0edb62deac6e7e7ccff5f
alexjj22griddynamics <arastvorov@griddynamics.com>
2019-12-23 11:19:38 +0200

    add logger

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const router = require('./routes')
 const app = new Koa();
 
 require('./handlers/01-favicon').init(app);
+require('./handlers/02-logger').init(app);
 require('./handlers/02-bodyParser').init(app);
 require('./handlers/03-templates').init(app);
 require('./handlers/04-session').init(app);
